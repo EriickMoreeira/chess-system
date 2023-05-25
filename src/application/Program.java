d66978d12eec7e7d6fc4ccfa8a96ed61920b2f5e
Erick Moreira <eriickmoreeira@gmail.com>
2023-05-25 17:49:20 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,9 +27,9 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
-				boolean[][] possibeMoves = chessMatch.possibleMoves(source);
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces(), possibeMoves);
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
 				
 				System.out.println();
